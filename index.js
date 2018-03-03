70b6c818041a6e8cd7b04345ce56d0075ff74a4a
John Gratton <jgratton@gmail.com>
2018-03-03 04:35:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
+
+function holidayCountdown(holiday, days){
+  returns `It's ${days} days until ${holiday}!`
+}
